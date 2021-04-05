dc76de649be0561937ad922856411dd18a346e48
neinkopp <jakob.schleitzer@gmail.com>
2021-04-05 16:17:46 +0200

    unset cookie maxAge

--- index.ts
+++ index.ts
@@ -54,7 +54,6 @@ const sess = {
 	cookie: {
 		httpOnly: true,
 		secure: false,
-		maxAge: 24 * 60 * 60 * 1000, // ms
 	},
 	secret: process.env.SESSION_COOKIE_SECRET
 		? process.env.SESSION_COOKIE_SECRET
